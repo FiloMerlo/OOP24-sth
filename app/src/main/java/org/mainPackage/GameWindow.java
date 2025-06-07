111fd8faea26bbcd736840802912a1b0b85ff84a
Francesco Giancaterino <144063511+francescogianca@users.noreply.github.com>
2025-06-07 20:15:55 +0200

    Add New GamePanel, GameLoop and GameWindow
    New architecture

---
+++ app/src/main/java/org/mainPackage/GameWindow.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+public class GameWindow extends JFrame {
+    private GamePanel gamePanel;
+    private Game game;
+
+public GameWindow(String title, GamePanel gamePanel, Game game) {
+        
+        super(title);
+        this.gamePanel = gamePanel;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // oppure DO_NOTHING_ON_CLOSE
+        setResizable(true);
+        add(gamePanel);
+        pack(); // adatta la finestra alla dimensione del pannello
+        setLocationRelativeTo(null); // centra la finestra
+        setVisible(true);
+        gamePanel.requestFocusInWindow();
+        
+        addWindowListener(new WindowAdapter() {
+            
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (game != null) {
+                    game.stop();
+                    System.out.println("Gioco chiuso correttamente");
+                } else {
+                    System.err.println("Errore in chiusura gioco");
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+}
+
