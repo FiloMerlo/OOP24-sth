3cda661481409a7602d9e3a5fda87415852aa14e
Samuele <samuelebizzocchi58@gmail.com>
2025-06-08 11:00:18 +0200

    RingPhysics fixed

--- app/src/main/java/org/mainPackage/components/PhysicsTypes/RingPhysics.java
+++ app/src/main/java/org/mainPackage/components/PhysicsTypes/RingPhysics.java
@@ -1,5 +1,6 @@
 package org.mainPackage.components.PhysicsTypes;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 
@@ -11,21 +12,36 @@ import org.mainPackage.game_parts.direction;
 
 public class RingPhysics extends Physics {
     private RingCollider collider;
-    public RingPhysics(Entity o, ArrayList<Rectangle> tList, PlayerPhysics pp){ 
+    private int maxDistance = 10;
+    private Point spawn;
+
+    public RingPhysics(Entity o, ArrayList<Rectangle> tList, PlayerPhysics pp, int xS, int yS){
         super(0, 0, o, tList);
         collider = new RingCollider(tList, this, pp);
+        spawn = new Point(xS, yS);
     }
 
     public void update(){
         collider.checkCollisions();
-        owner.moveX(xSpeed);
-        owner.moveY(ySpeed);
-        collider.getSensor().translate(xSpeed, ySpeed);
+        if (xSpeed > 0){
+            if (owner.getX() - spawn.getX() < maxDistance && owner.getX() - spawn.getX() > -maxDistance){
+                owner.moveX(xSpeed);
+            }
+            if (owner.getY() - spawn.getY() < maxDistance && owner.getY() - spawn.getY() > -maxDistance){
+                owner.moveY(xSpeed);
+            }        
+            collider.getSensor().translate(xSpeed, ySpeed);
+        }
     }
 
-    public void bounce(){
-        xSpeed = xSpeed * (-1);
-        ySpeed = ySpeed * (-1);
+    public void bounce(Rectangle tileMet){
+        if (tileMet.getY() >= owner.getY() + owner.getHeight() ||
+            tileMet.getY() <= owner.getY()){
+            ySpeed = -ySpeed;
+        }
+        else {
+            xSpeed = -xSpeed;
+        }
     }
 
     public void spredOut(){
