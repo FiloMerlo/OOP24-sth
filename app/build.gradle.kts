8ebad56f793bd3373fe5358edb5648e40828f934
Filippo Merlini <filippo.merlini2@studio.unibo.it>
2025-06-10 02:49:43 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.mainPackage.engine.App"
+    mainClass = "org.mainPackage.App"
 }
 
 tasks.named<Test>("test") {
