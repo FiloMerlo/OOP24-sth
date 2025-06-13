32f820ab6562e0eb6ad35fec00aa5d244bb384d0
Filippo Merlini <filippo.merlini2@studio.unibo.it>
2025-06-14 01:13:25 +0200

    Update InputComponent.java

--- app/src/main/java/org/mainPackage/engine/components/InputComponent.java
+++ app/src/main/java/org/mainPackage/engine/components/InputComponent.java
@@ -1,13 +1,42 @@
 package org.mainPackage.engine.components;
 
-import org.mainPackage.engine.events.impl.SubjectImpl;
+import org.mainPackage.engine.components.PhysicsTypes.PlayerPhysics;
+import org.mainPackage.engine.entities.api.Entity;
+import org.mainPackage.engine.events.api.Event;
+import org.mainPackage.engine.events.api.Observer;
+import org.mainPackage.engine.events.impl.InputEvent;
+import org.mainPackage.engine.systems.InputManager;
+import org.mainPackage.game_parts.direction;
 
-public class InputComponent extends SubjectImpl implements Component {
+import java.awt.event.KeyEvent;
 
+public class InputComponent implements Component, Observer{
+
+    Entity owner;
+    PlayerPhysics playerPhysics = owner.getComponent(PlayerPhysics.class);
+    // Polling through input manager when a key is down (Character running to right or left)
     @Override
-    public void update(float deltaTime) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public void update() {
+        if(InputManager.getInstance().isKeyDown(KeyEvent.VK_RIGHT)){
+            playerPhysics.moveX(direction.right);
+        }
+        if(InputManager.getInstance().isKeyDown(KeyEvent.VK_LEFT)){
+            playerPhysics.moveX(direction.left);
+        }
+    }
+
+    // Input manager fired the event, key is pressed, released within a fraction of a 'second'
+    @Override
+    public void onNotify(Event event) {
+        if (event instanceof InputEvent){
+            InputEvent i = (InputEvent) event;
+            if (playerPhysics == null){
+                return;
+            }
+            if (i.getKeyEvent().getKeyCode() == KeyEvent.VK_SPACE){
+                playerPhysics.jump();
+            }
+        }
     }
     
 }
