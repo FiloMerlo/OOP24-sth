69ade6ced62fb71b181c095a7c0776813ce0af8e
Francesco Giancaterino <frances.giancaterin2@studio.unibo.it>
2025-06-14 14:11:03 +0200

    Update GameStateManager.
    Added PlayingState methods

--- app/src/main/java/org/mainPackage/state_management/GameStateManager.java
+++ app/src/main/java/org/mainPackage/state_management/GameStateManager.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import org.mainPackage.util.SizeView;
 
+import org.mainPackage.engine.entities.api.Entity;
+
 /**
  * Gestisce i diversi stati del gioco (es. menu, gioco, pausa).
  * Si occupa di delegare l'aggiornamento e il disegno dello stato corrente. Delega anche gli input
@@ -19,6 +21,11 @@ public class GameStateManager {
     private SizeView sizeView;
     private Runnable shutdownGame;
 
+    /* Campi per il playingState */
+    private Entity sonicEntity;
+    private int[][] levelGrid;
+    private int tileWorldSize;
+
   
     public enum State {
         MENU,
@@ -32,7 +39,7 @@ public class GameStateManager {
         this.sizeView = sizeView;
         this.shutdownGame = shutdowGame;
 
-        playingState = new PlayingState(this, sizeView);
+        playingState = new PlayingState(this, sizeView, sonicEntity, levelGrid, tileWorldSize);
         pausedState = new PausedState(this, sizeView); // da passare il sizeview
         setState(State.MENU);
 
@@ -58,6 +65,33 @@ public class GameStateManager {
     
     public GameState getCurrentState() {
         return currentState;
+    }
+
+    
+    /* Metodi per playing state */
+    public void setSonicEntity(Entity Sonic) {
+        this.sonicEntity = sonic;
+    }
+
+    public void setLevelGrid(int[][] grid) {
+        this.levelGrid = grid;
+    }
+
+    public void setTileWorldSize(int size) {
+        this.tileWorldSize = size;
+    }
+
+    
+    public Entity getSonicEntity() {
+        return this.sonicEntity;
+    }
+
+    public int[][] getLevelGrid() {
+        return this.levelGrid;
+    }
+
+    public int getTileWorldSize() {
+        return this.tileWorldSize;
     }
 
     
