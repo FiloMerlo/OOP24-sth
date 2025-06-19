4ad076e153773fde97dcfc6f1aafaddf5fe171ab
Filippo <filippo.merlini2@studio.unibo.it>
2025-06-19 15:35:14 +0200

    Fixed PhysicsTest

--- app/src/test/java/org/mainPackage/engine/components/PhysicsTypes/PlayerPhysicsTest.java
+++ app/src/test/java/org/mainPackage/engine/components/PhysicsTypes/PlayerPhysicsTest.java
@@ -93,9 +93,8 @@ public class PlayerPhysicsTest {
         world.add(new Rectangle2D.Float(10, 14, 4, 4)); // piattaforma sotto Sonic
 
         sonic.addComponent(new TransformComponent(10, 10, 4, 4));
+        sonic.addComponent(new WalletComponent(world));
         sonic.addComponent(new PlayerPhysics(sonic, world));
-        sonic.addComponent(new WalletComponent(EntityManagerImpl.getInstance(), world));
-        
         enemy.addComponent(new TransformComponent(10, 10, 3, 3));
         enemy.addComponent(new EnemyPhysics(0, enemy, world, sonic));
 
